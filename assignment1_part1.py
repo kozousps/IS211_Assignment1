f7158658b2e6589853fe6c1303236f6e238f1bf0
kozousps <kou.omori90@spsmail.cuny.edu>
2021-02-02 16:12:03 -0500

    by k.o

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,37 @@
-
-def listDivide(numbers, divide):
+def listDivide(numbers, divide=2):
     """
-    The function returns the number of elements in the numbers list that are divisibleby divide
+    Creates listDivide that finds number of integers in list divisible by
+    divide (defaults to 2)
+    """
+    count = 0
+    for x in numbers:
+        if x % divide == 0:
+            count += 1
+    return(count)
+
+
+class ListDivideException(Exception):
+    """
+    Creates ListDivideException from all Exceptions
     """
     pass
+
 
 def testListDivide():
     """
     Test listDivide
     """
-    assert listDivide([1,2,3,4,5]) == 2
-    assert listDivide([2,4,6,8,10]) == 5
-    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
-    assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
-    
+    if listDivide([1, 2, 3, 4, 5]) != 2:
+        raise ListDivideException()
+    if listDivide([2, 4, 6, 8, 10]) != 5:
+        raise ListDivideException()
+    if listDivide([30, 54, 63, 98, 100], divide=10) != 2:
+        raise ListDivideException()
+    if listDivide([]) != 0:
+        raise ListDivideException()
+    if listDivide([1, 2, 3, 4, 5], 1) != 5:
+        raise ListDivideException()
+
+
 if __name__ == "__main__":
     testListDivide()
